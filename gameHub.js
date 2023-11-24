bbd2c3ef5b78e0b529bc9198babaa331f4a18cb0
Nathan Gillespie <112279301+CrookedTwig4@users.noreply.github.com>
2023-11-23 18:02:03 -0800

    Thanksgiving party

---
+++ gameHub.js
@@ -0,0 +1,26 @@
+function openBlackjack() {
+    window.open("blackjack.html", "Blackjack", "width=500,height=400,resizable=no");
+    window.close();
+}
+
+function openBrickBreaker() {
+    window.open("brickBreaker.html", "Brick Breaker", "width=500,height=400,resizable=no");
+    window.close();
+}
+
+function openTicTacToe() {
+    openNull();
+}
+
+function openFlappyBird() {
+    openNull();
+}
+
+function openNull() {
+    alert("No. -Rosa Parks");
+}
+
+function close() {
+    window.close();
+}
+
