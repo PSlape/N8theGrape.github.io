0e95ca0a123f60649f3ff01a7466a93082c02942
Nathan Gillespie <112279301+CrookedTwig4@users.noreply.github.com>
2023-11-24 22:52:55 -0800

    t

--- gameHub.js
+++ gameHub.js
@@ -1,23 +1,19 @@
-var gameWindow;
+
 function openBlackjack() {
-    gameWindow.close();
-    gameWindow = window.open("blackjack.html", "Blackjack", "width=500,height=400,resizable=no");
+    window.open("blackjack.html", "Blackjack", "width=500,height=400,resizable=no");
     self.close();
 }
 
 function openBrickBreaker() {
-    gameWindow.close();
-    gameWindow = window.open("brickBreaker.html", "Brick Breaker", "width=500,height=400,resizable=no");
+    window.open("brickBreaker.html", "Brick Breaker", "width=500,height=400,resizable=no");
     self.close();
 }
 
 function openTicTacToe() {
-    gameWindow.close();
     openNull();
 }
 
 function openFlappyBird() {
-    gameWindow.close();
     openNull();
 }
 
